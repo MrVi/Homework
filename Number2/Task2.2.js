e4c63857f81576dd993323dc0908ccb7a51783b6
Mr Vi <allesta100@gmail.com>
2017-03-17 10:19:22 +0300

    hw2 task2.2mod

--- Number2/Task2.2.js
+++ Number2/Task2.2.js
@@ -1,11 +1,40 @@
 var arr1=[9,6,4,3,2];
 var arr2=[1,2,3,4,5];
 
+//Var1
 var result = arr1.filter(function(n) {
     return arr2.indexOf(n) > -1;
 });
 alert(result);
 
+//Var2
+
+var arr1=[9,6,4,3,2];
+var arr2=[3,4,1,5,2];
+var result=[];
+
+arr1.sort();
+alert(arr1);
+
+function BinarySearch(t,A){
+    var i = 0, j = A.length, k=0;
+    while (i < j) {
+        k = Math.floor((i+j)/2);
+        if (t <= A[k]) {j = k;}
+        else {i = k+1;}
+    }
+    return ((A[ i ] === t) ? i: -1);
+}
+
+for (var i=0;i<arr2.length; i++){
+    console.log(BinarySearch(arr2[i], arr1));
+    if (BinarySearch(arr2[i], arr1) >= 0){
+        result.push(arr2[i]);
+    }
+}
+
+alert("res" + result);
+//Var3
 function IntersecArrays(A,B) {
     var  c = 0, C = [];
     for (var i = 0; i < A.length; i++) {
